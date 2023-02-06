631673b9cd07224c98ee753075c4313c566009d1
susana <susafp@hotmail.com>
2023-02-06 19:55:54 +0000

    fix projects link for small screens

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const gradient = document.querySelector(".gradient");
 
+
 function menuOnClick() {
     document.getElementById("menu-bar").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
